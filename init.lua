7bd869722916a092ecd41b6355363eebd49a5305
Oscar Román <oscar.roman@mundotelecomunicaciones.cl>
2023-03-31 21:58:18 -0300

    chore: Added custom plugins && mappings
    I added vim-visual-multi pluggin and config mapping  to load it in some file

---
+++ init.lua
@@ -0,0 +1,2 @@
+--local plugins = { "mg979/vim-visual-multi" } return plugins
+return { "mg979/vim-visual-multi" }
---
+++ plugins.lua
@@ -0,0 +1,1 @@
+local plugins = { { "mg979/vim-visual-multi", ft = {"js","text","html","txt","sql","php","xml","wsdl"} } } return plugins
