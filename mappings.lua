04dc167c923592de7d351e52eb36bbfa835e0540
Oscar Román <oscar.roman@mundotelecomunicaciones.cl>
2023-04-03 20:19:50 -0400

    chore: Add mappping for format code.
    Added keymappings with <S-A-f>

--- mappings.lua
+++ mappings.lua
@@ -1,30 +1,18 @@
---local map = require("utils").map map("n", "<C-S-L>", "<Plug>(VM-Select-Word)")
+local mappings = {
+  -- first key is the mode
+  -- desc setting is stored by vim.keymap.set() as a part of opts table in vim lua module
+  n = {
+    ["<S-A-f>"] = {
+      function() vim.lsp.buf.format(M.format_opts) end,
+      desc = "Format buffer",
+    }
+  },
+  t = {
+    -- setting a mapping to false will disable it
+    ["<esc>"] = false,
+  },
+} 
 
--- Mapping data with "desc" stored directly by vim.keymap.set().
---
--- Please use this mappings table to set keyboard mapping since this is the
--- lower level configuration and more robust one. (which-key will
--- automatically pick-up stored data by this setting.)
-return {
-    -- first key is the mode
-    n = {
-      -- second key is the lefthand side of the map
-      -- mappings seen under group name "Buffer"
-      ["<leader>bn"] = { "<cmd>VM-Select-Word<cr>", desc = "New tab" },
-      ["<leader>bD"] = {
-        function()
-          require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
-        end,
-        desc = "Pick to close",
-      },
-      -- tables with the `name` key will be registered with which-key if it's installed
-      -- this is useful for naming menus
-      ["<leader>b"] = { name = "Buffers" },
-      -- quick save
-      -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
-    },
-    t = {
-      -- setting a mapping to false will disable it
-      -- ["<esc>"] = false,
-    },
-  }
+
+return mappings;
+
--- plugins.lua
+++ plugins.lua
@@ -1,1 +1,15 @@
-local plugins = { { "mg979/vim-visual-multi", ft = {"js","text","html","txt","sql","php","xml","wsdl"} } } return plugins
+-- local utils = require "astronvim.utils"
+-- local is_available = utils.is_available
+-- if is_available "toggleterm.nvim" then
+--     require("toggleterm.terminal").Terminal:new({shell = "powershell.exe"})
+--     -- require("toggleterm.nvim").setup({shell = "powershell.exe"})
+-- end
+
+local plugins = { 
+    { "mg979/vim-visual-multi", ft = {"lua","js","text","html","txt","sql","php","xml","wsdl"} },
+    {
+        "toggleterm.nvim",
+        shell = "C:/Users/odrom/AppData/Local/Microsoft/WindowsApps/Microsoft.PowerShell_8wekyb3d8bbwe/pwsh.exe"
+      }
+
+} return plugins
