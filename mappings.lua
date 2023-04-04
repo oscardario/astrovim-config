e72d3187e8b3b98eaa8ee92dff11f8917297eabd
Oscar Román <oscar.roman@mundotelecomunicaciones.cl>
2023-04-04 10:46:05 -0400

    chore: Modify mapping to search files
    Custom key mapping for search files with <C-p>

--- mappings.lua
+++ mappings.lua
@@ -5,7 +5,8 @@ local mappings = {
     ["<S-A-f>"] = {
       function() vim.lsp.buf.format(M.format_opts) end,
       desc = "Format buffer",
-    }
+    },
+    ["<C-p>"] = { function() require("telescope.builtin").find_files() end, desc = "Find files" }
   },
   t = {
     -- setting a mapping to false will disable it
