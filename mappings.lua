a72f59ac9c52ce999b67e548a8edc7e3cc594751
Oscar Román <oscar.roman@mundotelecomunicaciones.cl>
2023-05-20 01:03:04 -0400

    feat: add some config for vim-visual-multi and instaling plug codeium

--- mappings.lua
+++ mappings.lua
@@ -12,11 +12,10 @@ local mappings = {
     -- setting a mapping to false will disable it
     ["<esc>"] = false,
   },
-} 
+}
 -- Go to the end of the start and end
-vim.api.nvim_set_keymap('n', '0', '<End>', {noremap=true})
+vim.api.nvim_set_keymap('n', '0', '<End>', { noremap = true })
 -- Go to the begin of and end
 vim.api.nvim_set_keymap('n', '9', '^', { noremap = true, silent = true })
 
 return mappings;
-
--- plugins.lua
+++ plugins.lua
@@ -5,11 +5,46 @@
 --     -- require("toggleterm.nvim").setup({shell = "powershell.exe"})
 -- end
 
-local plugins = { 
-    { "mg979/vim-visual-multi", ft = {"lua","text","html","txt","sql","php","jsx","ts","xml","wsdl","javascript"} },
-    {
-        "toggleterm.nvim",
-        shell = "C:/Users/odrom/AppData/Local/Microsoft/WindowsApps/Microsoft.PowerShell_8wekyb3d8bbwe/pwsh.exe"
+local plugins = {
+  {
+    "mg979/vim-visual-multi",
+    ft = { "lua", "text", "html", "txt", "sql", "php", "jsx", "ts", "xml", "wsdl",
+      "javascript" }
+  },
+  {
+    "toggleterm.nvim",
+    shell = "C:/Users/odrom/AppData/Local/Microsoft/WindowsApps/Microsoft.PowerShell_8wekyb3d8bbwe/pwsh.exe"
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          show_hidden_count = true,
+          hide_dotfiles = false,
+          hide_gitignored = true,
+          hide_by_name = {
+            'node_modules/*',
+            '.git',
+            '.DS_Store',
+            'thumbs.db',
+          },
+          never_show = {},
+        },
       }
-
-} return plugins
+    }
+  },
+  {
+    'Exafunction/codeium.vim',
+    lazy = false,
+    config = function()
+      -- Change '<C-g>' here to any keycode you like.
+      vim.keymap.set('i', '<C-g>', function() return vim.fn['codeium#Accept']() end, { expr = true })
+      vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
+      vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
+      vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true })
+    end
+  }
+}
+return plugins
